fec19512e1586fb55b4eb86a312b3af2b79f23d8
leb <doncaleb05@gmail.com>
2025-04-30 09:14:22 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,20 @@
-
 const CACHE_NAME = "time-machine-cache-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/9953794.png",
-  "/9953794-fotor-2025042914323.png"
+  "./",               // caches the folder’s default document (index.html)
+  "./index.html",
+  "./manifest.json",
+  "./9953794-fotor-2025042914323.png",
+  "./9953794.png"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event =>
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  )
+);
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event =>
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  )
+);
