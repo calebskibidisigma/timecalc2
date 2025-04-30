76fa21cffa5e70c55d684948e63747485a520da2
leb <doncaleb05@gmail.com>
2025-04-30 09:19:46 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,28 @@
+// service-worker.js
 const CACHE_NAME = "time-machine-cache-v1";
 const urlsToCache = [
-  "./",               // caches the folder’s default document (index.html)
+  "./",             // caches your repo’s root (index.html)
   "./index.html",
   "./manifest.json",
   "./9953794-fotor-2025042914323.png",
   "./9953794.png"
 ];
 
-self.addEventListener("install", event =>
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  )
-);
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+});
 
-self.addEventListener("fetch", event =>
+self.addEventListener("fetch", event => {
+  // If this is a navigation to a new page, serve index.html from cache
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      caches.match("./index.html").then(cached => cached || fetch(event.request))
+    );
+    return;
+  }
+
+  // Otherwise, try cache first then network
   event.respondWith(
     caches.match(event.request).then(resp => resp || fetch(event.request))
-  )
-);
+  );
+});
